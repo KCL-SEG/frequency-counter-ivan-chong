f2c45f80e2b3185895c441a75c3cb1aba56a2bf9
Ka Chun Chong <ka.c.chong@kcl.ac.uk>
2022-09-24 15:50:20 +0100

    Add initial implementation of the counter
    This uses iteration over the mapped list and either creates an entry of
    1 in the dictionary for the key, or if exists, increments the value of
      the key.

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,10 @@
 
 def frequencies(items):
     frequencies = {}
+    for item in map(str, items):
+        if item in frequencies:
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
     # Your code goes here
     return frequencies
